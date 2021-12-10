2ced84ba8220fca30e8821b0fc60a9fc3581cc07
sdz <sdz_zju@126.com>
2021-12-10 20:27:46 +0800

    finsh client.py

--- game/src/client.py
+++ game/src/client.py
@@ -6,8 +6,10 @@ from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
+# python读取命令行参数包
+from sys import stdin
 
-def main():
+def operate(op, user_id, username, score):
     # Make socket
     transport = TSocket.TSocket('localhost', 9090)
 
@@ -24,11 +26,19 @@ def main():
     transport.open()
 
     # 具体业务代码
-    user = User(1, 'sdz', 1500)
-    client.add_user(user, "")
+    user = User(user_id, username, score)
+    if op == "add":
+        client.add_user(user, "")
+    elif op == "remove":
+        client.remove_user(user, "")
 
     # Close!
     transport.close()
+
+def main():
+    for line in stdin:
+        op, user_id, username, score = line.split(' ')
+        operate(op, int(user_id), username, int(score))
 
 if __name__ == "__main__":
     main()
